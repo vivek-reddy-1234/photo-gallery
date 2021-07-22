d6f690a0e68855b183070793e761271abf9be466
vivek-reddy-1234 <vivek.reddy@wissen.com>
2021-07-22 12:01:54 +0530

    added login tab

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButtons, IonMenuButton, IonRow, IonCol, IonButton, IonList, IonItem, IonLabel, IonInput, IonText } from '@ionic/react';
+
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -7,16 +8,53 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="small">Login</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        {/* <ExploreContainer name="login page" /> */}
+        <form noValidate onSubmit={() => {}}>
+          <IonList>
+            <IonItem>
+              <IonLabel position="stacked" color="primary">Username</IonLabel>
+              <IonInput name="username" type="text" value={""} spellCheck={false} autocapitalize="off" onIonChange={e => {}}
+                >
+              </IonInput>
+            </IonItem>
+
+            {/* {formSubmitted && usernameError && <IonText color="danger">
+              <p className="ion-padding-start">
+                Username is required
+              </p>
+            </IonText>} */}
+
+            <IonItem>
+              <IonLabel position="stacked" color="primary">Password</IonLabel>
+              <IonInput name="password" type="password" value={''} onIonChange={e => {}}>
+              </IonInput>
+            </IonItem>
+
+            {/* {formSubmitted && passwordError && <IonText color="danger">
+              <p className="ion-padding-start">
+                Password is required
+              </p>
+            </IonText>} */}
+          </IonList>
+
+          <IonRow>
+            <IonCol>
+              <IonButton type="submit" expand="block">Login</IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton routerLink="/signup" color="light" expand="block">Signup</IonButton>
+            </IonCol>
+          </IonRow>
+        </form>
       </IonContent>
     </IonPage>
   );
